cd36b9872bed71fec191cddb3acf34959421e43f
Danielcard99 <dc2651065@gmail.com>
2025-04-03 14:50:30 -0300

    refactor(models): remove updatedAt from CarsItem model

--- models/CarsItem.js
+++ models/CarsItem.js
@@ -2,22 +2,26 @@ import { DataTypes } from "sequelize";
 import db from "../db/connection.js";
 import Cars from "./Cars.js";
 
-const CarsItem = db.define("cars_items", {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  car_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: Cars,
-      key: "id",
+const CarsItem = db.define(
+  "cars_items",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    onUpdate: "CASCADE",
-    onDelete: "RESTRICT",
+    car_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Cars,
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "RESTRICT",
+    },
   },
-});
+  { timestamps: true, updatedAt: false }
+);
 
 Cars.hasMany(CarsItem, { foreignKey: "car_id" });
 CarsItem.belongsTo(Cars, { foreignKey: "car_id" });

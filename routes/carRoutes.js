9c3ccecc3dea76ff803272f712f6ebff381c4c12
Danielcard99 <dc2651065@gmail.com>
2025-06-25 23:36:55 -0300

    refactor(carRoutes): remove all comments for cleaner code

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -13,22 +13,16 @@ import {
 
 const router = express.Router();
 
-// Route to create a new car
 router.post("/", validateCarData, createCar);
 
-// Route to add or replace items for a car by ID
 router.put("/:id/items", validateItems, addItemsToCar);
 
-// Route to get a specific car by ID, including its items
 router.get("/:id", getCarById);
 
-// Route to get a paginated list of cars with optional filters
 router.get("/", getCars);
 
-// Route to partially update a car by ID
 router.patch("/:id", validatePartialUpdate, updateCar);
 
-// Route to delete a car and its associated items by ID
 router.delete("/:id", deleteCar);
 
 export default router;
